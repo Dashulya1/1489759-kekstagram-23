f6dfb4034435836e185d5827d4191b16a07ed74d
Dasha <dariasiluyanowa@gmail.com>
2021-06-12 07:09:11 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -19,3 +19,77 @@ const checkLenght = (comment, maxNum) => {
 
 randomInteger();
 checkLenght();
+
+const NAMES = [
+  'Саша',
+  'Виолетта',
+  'Катя',
+  'Полли',
+  'Жора',
+  'Паша',
+  'Андрей',
+  'Ноэль',
+  'Костя',
+];
+
+const MESSAGETEXT = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const idNum = [200];
+
+for (int = 0, int < idNum.length, int++) {
+  firstNum = 1;
+  idNum[int] = firstNum;
+  firstNum += 1;
+}
+
+const image = [
+  'avatar-1.svg',
+  'avatar-2.svg',
+  'avatar-3.svg',
+  'avatar-4.svg',
+  'avatar-5.svg',
+  'avatar-6.svg',
+];
+
+const description = [
+  'Выходные Кекса',
+  'Сегодня на дессерт сметанка',
+  'Вид из окна как всегда шикарный',
+  'Рыбка сегодня особенно прекрасна',
+  'Как же хочется поспать',
+];
+
+const commentDesc = function() {
+  return {
+    id: idNum[randomInteger(0, 134)],
+    avatar: `img/avatar-${image[randomInteger(0, 5)]}.svg`,
+    message: MESSAGETEXT[randomInteger(0, MESSAGETEXT.length - 1)],
+    name: NAMES[randomInteger(0, NAMES.length - 1)],
+  };
+};
+
+const comments = new Array(135).fill(null);
+
+comments.forEach (commentDesc());
+
+const createPhotoDescription = function() {
+  return {
+    id: idNum[randomInteger(0, 5)],
+    url: `photos/${idNum[randomInteger(0, 24)]}.jpg`,
+    description: description[randomInteger(0, description.length - 1)],
+    likes: idNum[randomInteger(0, 199)],
+    comments: comments[randomInteger(0, MESSAGETEXT.length - 1)],
+  };
+};
+
+const photoDescription = [];
+
+photoDescription;
+createPhotoDescription();
