737f2b51290e8fc5815cafbc6cbf31e8f53c1f17
Dasha <dariasiluyanowa@gmail.com>
2021-06-28 13:52:01 +0300

    Добавила переменную для шаблона

--- js/showMiniature.js
+++ js/showMiniature.js
@@ -5,12 +5,6 @@ import { createMiniature } from './data.js';
 const usersList = document.querySelector('.picture');
 //переменная для шаблона
 const usersPhotoTemplate = document.querySelector('#picture').content;
-//заводим переменную для url из шаблона
-const addressOfPicture = usersPhotoTemplate.querySelector('.picture__img');
-//заводим переменную для количества лайков из шаблона
-const likesOfPicture = usersPhotoTemplate.querySelector('.picture__likes');
-//заводим переменную для комментариев из шаблона
-const commentsOfPicture = usersPhotoTemplate.querySelector('.picture__comments');
 
 //заводим переменную, которая хранит коллекцию объектов
 const usersData = createMiniature();
@@ -20,15 +14,15 @@ const usersListFragment = document.createDocumentFragment();
 //Функция, которая генерирует нужные данные из объектов
 usersData.forEach(({url, likes, comments}) => {
   //клонируем шаблон
-  usersPhotoTemplate.cloneNode(true);
+  const userData = usersPhotoTemplate.cloneNode(true);
   //берем из объекта url
-  addressOfPicture.setAttribute('src', url);
+  userData.querySelector('.picture__img').setAttribute('src', url);
   //берем количество лайков
-  likesOfPicture.textContent = likes;
+  userData.querySelector('.picture__likes').textContent = likes;
   //берем комментарий
-  commentsOfPicture.textContent = comments;
+  userData.querySelector('.picture__comments').textContent = comments;
   //все скдладываем в шаблон
-  usersListFragment.appendChild(usersPhotoTemplate);
+  usersListFragment.appendChild(userData);
 });
 
 //добавляем в блок разметки
