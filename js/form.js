f13bd92c455c0f71733192e70d6f5737173bfc7c
Dasha <dariasiluyanowa@gmail.com>
2021-07-10 18:13:17 +0300

    Валидация комментария и хэштега

---
+++ js/form.js
@@ -0,0 +1,42 @@
+// Загрузка нового изображения:
+// выбор файла с изображением для загрузки;
+// изменение масштаба изображения;
+// применение одного из заранее заготовленных эффектов;
+// выбор глубины эффекта с помощью ползунка;
+// добавление текстового комментария;
+// добавление хэш-тегов.
+const formWindow = document.querySelector('.img-upload__overlay');
+const uploadPhoto = document.querySelector('.img-upload__photo');
+const uploadFile = document.querySelector('#upload-file');
+const ESCKEY = 27;
+
+function openEditor () {
+  formWindow.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+}
+
+function closeEditor () {
+  uploadFile.value = '';
+  formWindow.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  uploadPhoto.addEventListener('click', closeEditor);
+  document.addEventListener('keydown', pushEscKeydown);
+}
+
+function pushEscKeydown(evt) {
+  if (evt.keyCode === ESCKEY) {
+    closeEditor();
+  }
+}
+
+const uploadForm = function () {
+  openEditor();
+  // changeScale();
+  // imposeAnEffect();
+  // effectDepth();
+  // addComment();
+  // addHashTags();
+};
+
+uploadFile.addEventListener('change', uploadForm);
---
+++ js/validation.js
@@ -0,0 +1,53 @@
+const hashtagInput = document.querySelector('.text__hashtags');
+const textOfComment = document.querySelector('.text__description');
+
+const hashtagValidation = function (evt) {
+  const hashtagsArray = hashtagInput.value.split(' ');
+
+  if (hashtagsArray.lenght > 5) {
+    evt.target.setCustomValidity('Можно ввести не более 5 хэштегов');
+  }
+
+  hashtagsArray.forEach((hashtag) => {
+    const regularExpression = /^#[A-Za-zА-Яа-я0-9]{1,19}$/;
+
+    if (!regularExpression.test(hashtag)) {
+      evt.target.setCustomValidity('Хэштег начинается с #, может содержать только буквы и цифры и разделяется только пробелом');
+    }
+
+    if (hashtag.includes('#', 1)) {
+      evt.target.setCustomValidity('Хэштэг должен содержать только один #');
+    }
+  });
+
+  for (let i = 0; i < hashtagsArray.lenght; i++) {
+    let hash = hashtagsArray[i];
+    let switchHash = hashtagsArray[i + 1];
+
+    if (!hash.target.toLowerCase() === switchHash.target.toLowerCase()) {
+      hash = hashtagsArray[i + 1];
+      switchHash = hashtagsArray[i];
+    } else {
+      evt.target.setCustomValidity('Хэштеги не должны повторяться');
+    }
+  }
+};
+
+hashtagInput.addEventListener ('change', hashtagValidation);
+
+//валидация комментария
+const commentValidation = function (evt) {
+  const commentLenght = textOfComment.value.lenght;
+  const MIN_LENGHT = 2;
+  const MAX_LENGHT = 140;
+
+  if (commentLenght < MIN_LENGHT) {
+    evt.target.setCustomValidity(`Ещё ${MIN_LENGTH - commentLength} символов`);
+  } else if (commentLenght > MAX_LENGHT) {
+    evt.target.setCustomValidity(`Превышение на ${MAX_LENGHT - commentLenght} символов`);
+  } else {
+    evt.target.setCustomValidity = '';
+  }
+};
+
+textOfComment.addEventListener('input', commentValidation);
